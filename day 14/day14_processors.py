6d291e85bd7fba4ad002a0437709a706e284e76e
Muhammad Arslan <muhammadarslan23156@gmail.com>
2026-01-27 20:17:52 +0500

    I'm half way through some practice for sqlite3, not much to add in this message, i'll add when the task is fully complete.

---
+++ day 14/day14_processors.py
@@ -0,0 +1,53 @@
+import logging
+from exceptions import InvalidDataError
+from day14_base_processor import BaseProcessor
+
+class DataProcessor(BaseProcessor):
+    def __init__(self, data):
+    # We store the strategy "in our pocket" to use later
+    
+    
+    # Call the parent (BaseProcessor) to handle validation and self.data setup
+        super().__init__(data)
+        
+    def _validate(self, data):
+        """
+        Constructor: This runs when you create the object.
+        self.data is now a property of the object.
+        """
+        if not isinstance(data, list):
+            logging.critical("The data is not a list")
+            raise InvalidDataError("Data must be a list")
+        for item in data:
+            if not isinstance(item, (int, float)):
+                logging.error(f"The entered data {item} is not valid")
+                raise InvalidDataError("Data must only contain numbers")
+            
+        self.data = data
+        logging.info(f"DataProcessor initialized with {len(data)} items.")
+
+ # --- NEW MAGIC METHODS (TODO) ---
+
+    def __str__(self):
+        return f"DataProcessor with {len(self.data)} items"
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self, index):
+        return self.data[index]
+    
+ # --- Normal calculative METHODS/Method (TODO) ---
+    def process(self):
+        if not self.data:
+            return {
+                "count_positive": 0,
+                "average": None,
+                "min": None,
+                "max": None
+            }
+        positives = [n for n in self.data if n > 0]
+        return {
+            "positives": len(positives),
+            "average": sum(self.data) / len(self.data),
+            "min": min(self.data),
+            "max": max(self.data)
+        }  
